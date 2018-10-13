8004b0d611006cd71dec8dd5bf9911466f00c5c6
Sam <selfa97@outlook.com>
2018-10-13 12:14:04 +0800

    Search results now display, added qid passing to links.

--- webapp/app_server/controllers/search.js
+++ webapp/app_server/controllers/search.js
@@ -4,20 +4,6 @@ var ctrlMain = require('../controllers/main');
 
 /*--------Raymond's modification starts--------------*/
 
-//Return question information in search result webpage
-function search_results(matched_q_id){
-	var results=[];
-	for (var i=0;i<matched_q_id.length;i++){
-		var single_result = [];
-		connection.query('SELECT short_description,update_date,preview_path FROM Question WHERE q_id=?',[matched_q_id[i]],function(err,results){
-			if(err) throw err;
-			single_result.push(results[0]['short_description'],results[0]['update_date'],results[0]['preview_path']);
-		});
-	  results.push(single_result);
-	}
-	return (results);
-}
-
 //Return question history
 function single_question_history(q_id){
 	var result =[];
@@ -53,16 +39,20 @@ module.exports.result = async function(req, res) {
 	for(var i = 0; i < keywords.length; i++) {
 		keyword = "%".concat(keywords[i],"%");
 		//Search through database asynchronously
-		let searchQuery = await ctrlMain.queryPromise('SELECT q_id from Question WHERE key_words LIKE ?',[keyword]);
+		let searchQuery = await ctrlMain.queryPromise('SELECT q_id,short_description,update_date from Question WHERE key_words LIKE ?',[keyword]);
 
 		//For array of returned qids, check if it's already in results -> if not add to reulst
 		for (var j=0;j<searchQuery.length;j++){
 			if(results.includes(searchQuery[j]['q_id'])==false){
-				results.push(searchQuery[j]['q_id']);
+				temp = new Array(3);
+				temp[0] = searchQuery[j]['q_id'];
+				temp[1] = searchQuery[j]['short_description'];
+				temp[2] = searchQuery[j]['update_date'].getFullYear();
+				results.push(temp);
 			}
 		}
 	}
 
 	console.log(results);
-	res.render('result', {});
+	res.render('result', {results: results});
 };
