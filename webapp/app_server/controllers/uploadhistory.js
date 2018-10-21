6ded22ef894e0fa1d00e3fb951dcbc3a0b9c5088
Lachlord <lachlanabunney27827@gmail.com>
2018-10-21 14:38:47 +0800

    tried pass qid to uploadhistory
    passes the qid to uploadhistory, I think

--- webapp/app_server/controllers/uploadhistory.js
+++ webapp/app_server/controllers/uploadhistory.js
@@ -5,11 +5,13 @@ var readChunk = require('read-chunk');
 
 module.exports.uploadhistory = async function(req,res){
   var formno = req.query.formno;
+  var qid = req.query.qid;
   var userPapers = ["Testing Paper Pls Show"];
   var user_id = req.session.user;
   let testQuery = await ctrlMain.queryPromise('SELECT institution, unit, assessment, date FROM Paper WHERE user_id = ?',[user_id]);
   for (var i = 0; i < testQuery.length; i++) {
     userPapers.push(testQuery[i]['institution'] +" "+ testQuery[i]['unit'] +" "+  testQuery[i]['assessment'] +" "+ testQuery[i]['date'] +" "+ testQuery[i]['proportion']);
   }
-  res.render('uploadhistory',{formno: formno, papers: userPapers});
+  res.render('uploadhistory',{formno: formno, papers: userPapers, qid: qid});
+
 }
