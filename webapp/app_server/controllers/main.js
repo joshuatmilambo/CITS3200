f7cbb1423678fe6ced294dce84db23b9797dce97
Sam <selfa97@outlook.com>
2018-10-21 21:23:58 +0800

    Rando push

--- webapp/app_server/controllers/main.js
+++ webapp/app_server/controllers/main.js
@@ -1,4 +1,5 @@
 var ctrlMain = require('../controllers/main');
+var formidable = require('formidable');
 const express = require('express');
 var mysql = require('mysql');
 
@@ -16,24 +17,21 @@ module.exports.addnewuser = function(req,res){
   var username=req.body.username;
   var password=req.body.password;
   var userexist=false;
-  connection.connect(function(err) {
-  	if (err) throw err;
-  	connection.query('select * from user',function(err,result,fields){
-      if(err) throw err;
-      for(i=0;i<result.length;i++){
-        if(username==result[i].user_name){
-          userexist=true;
-        }
+  connection.query('select * from user',function(err,result,fields){
+    if(err) throw err;
+    for(i=0;i<result.length;i++){
+      if(username==result[i].user_name){
+        userexist=true;
       }
-      if(userexist){
-        res.render('adduser',{warning:'User already exist'});
-      }else{
-        connection.query("insert into user (user_name,user_type) values ('"+username+"','"+password+"')",function(err,result,fields){
-          if (err) throw err;
-          res.render('adduser',{warning:'User sucessfully added'})
-        });
-      }
-    });
+    }
+    if(userexist){
+      res.render('adduser',{warning:'User already exist'});
+    }else{
+      connection.query("insert into user (user_name,user_type) values ('"+username+"','"+password+"')",function(err,result,fields){
+        if (err) throw err;
+        res.render('adduser',{warning:'User sucessfully added'})
+      });
+    }
   });
 };
 
@@ -41,17 +39,14 @@ module.exports.addnewuser = function(req,res){
 module.exports.delete = function(req,res){
   var user=req.session.user;
   var qid=req.query.q;
-  connection.connect(function(err) {
-    if (err) throw err;
-      connection.query('delete from Temp_Paper where user_id="'+user+'" and q_id="'+qid+'"',function(err,result,fields){
+    connection.query('delete from Temp_Paper where user_id="'+user+'" and q_id="'+qid+'"',function(err,result,fields){
+      if(err) throw err;
+      connection.query('select * from Temp_Paper join question on Temp_Paper.q_id=question.q_id where user_id="'+user+'"',function(err,result,fields){
         if(err) throw err;
-        connection.query('select * from Temp_Paper join question on Temp_Paper.q_id=question.q_id where user_id="'+user+'"',function(err,result,fields){
-          if(err) throw err;
-          console.log(result);
-          res.render('test',{test:result});
-        });
+        console.log(result);
+        res.render('test',{test:result});
       });
-  });
+    });
 };
 
 
@@ -68,14 +63,11 @@ module.exports.upload = function(req,res){
 /* GET test page */
 module.exports.test = function(req,res){
   var user=req.session.user;
-    connection.connect(function(err) {
-      if (err) throw err;
-        connection.query('select * from Temp_Paper join question on Temp_Paper.q_id=question.q_id where user_id="'+user+'"',function(err,result,fields){
-          if(err) throw err;
-          console.log(result);
-          res.render('test',{test:result});
-      });
-});
+  connection.query('select * from Temp_Paper join question on Temp_Paper.q_id=question.q_id where user_id="'+user+'"',function(err,result,fields){
+    if(err) throw err;
+    console.log(result);
+    res.render('test',{test:result});
+  });
 };
 
 module.exports.questionadded = async function (req, res) {
@@ -87,15 +79,13 @@ module.exports.questionadded = async function (req, res) {
 
 /* GET history page */
 module.exports.history = async function (req, res) {
-
-    var results = []//['UWA', 'Physics', 'test', '05/09/2016', '100','200','used copper' , 'UWA', 'Physics', 'test', '12/09/2015', '5','10','null'];
-    let testQuery = await ctrlMain.queryPromise('SELECT institution, unit, assessment, date, correct, total_student, note FROM question_history q JOIN paper p WHERE q.paper_id = p.paper_id AND q_id = 3');
-    for (var i = 0; i < testQuery.length; i++) {
-        results.push(testQuery[i]['institution'], testQuery[i]['unit'], testQuery[i]['assessment'], testQuery[i]['date'], testQuery[i]['correct'], testQuery[i]['total_student'], testQuery[i]['note']);
-    }
-    var input = req.query.qid;
-    console.log(results, input);
-    res.render('history', {results:results});
+  var qid = req.query.q;
+  var results = []//['UWA', 'Physics', 'test', '05/09/2016', '100','200','used copper' , 'UWA', 'Physics', 'test', '12/09/2015', '5','10','null'];
+  let testQuery = await ctrlMain.queryPromise('SELECT institution, unit, assessment, date, correct, total_student, note FROM question_history q JOIN paper p WHERE q.paper_id = p.paper_id AND q_id = ' + qid);
+  for (var i = 0; i < testQuery.length; i++) {
+      results.push(testQuery[i]['institution'], testQuery[i]['unit'], testQuery[i]['assessment'], testQuery[i]['date'], testQuery[i]['correct'], testQuery[i]['total_student'], testQuery[i]['note']);
+  }
+  res.render('history', {results:results});
 }
 
 /* Get test history page */
@@ -104,13 +94,10 @@ module.exports.testhistory = function(req,res){
   console.log(userid);
   var testhistories;
   var username=req.session.user
-  connection.connect(function(err) {
-    if (err) throw err;
-    connection.query('select * from paper where user_id="'+userid+'"',function(err,result,fields){
-      if(err) throw err;
-      //console.log(result);
-      res.render('testhistory',{testhis:result});
-  });
+  connection.query('select * from paper where user_id="'+userid+'"',function(err,result,fields){
+    if(err) throw err;
+    //console.log(result);
+    res.render('testhistory',{testhis:result});
 });
 };
 
@@ -121,36 +108,61 @@ module.exports.updateresults = function(req,res){
   var username=req.session.user;
   console.log(pid);
   console.log(username);
-  connection.connect(function(err) {
-    if (err) throw err;
-    connection.query('select * from ((question_history join question on question_history.q_id=question.q_id) join paper on question_history.paper_id=paper.paper_id) where paper.paper_id="'+pid+'"',function(err,result,fields){
-      //console.log(result);
-      if(err) throw err;
-      res.render('updateresult',{questions:result,paper_id:pid});
-      //console.log(result);
+  connection.query('select * from ((question_history join question on question_history.q_id=question.q_id) join paper on question_history.paper_id=paper.paper_id) where paper.paper_id="'+pid+'"',function(err,result,fields){
+    //console.log(result);
+    if(err) throw err;
+    res.render('updateresult',{questions:result,pid:pid});
+    //console.log(result);
   });
-});
 };
 
 
 // Update result
 module.exports.update = function(req,res){
-  var n;
-  var correct=req.body.correct;
-  var total=req.body.total;
-  var qid=req.body.qid;
-  console.log(qid);
-  var pid=req.query.p;
-  connection.connect(function(err) {
-    if (err) throw err;
-    for (n=0; n<correct.length;n++){
-      console.log(qid[n]);
-      connection.query('update question_history set correct="'+correct[n]+'",total_student="'+total[n]+'" where q_id="'+qid[n]+'"'),function(err,result,fields){
-        if(err) throw err;
+  var form = new formidable.IncomingForm(),
+      fields = [];
+
+  form
+    .on('field', function(field, value) {
+      console.log(field, value)
+      fields.push([field, value]);
+    })
+
+    .on('end', async function() {
+      var data = [];
+
+      for(var i = 0; i < fields.length; i++) {
+        if(i%2 == 0) data.push(new Array(2));
+
+        var num = fields[i][0].split('.')[1]
+
+        if(/^correct/.test(fields[i][0])) {
+          data[num][0] = fields[i][1];
+        }
+        else if(/^total/.test(fields[i][0])) {
+          data[num][1] = fields[i][1];
+        }
+      }
+
+      var qid=req.body.qid;
+      var pid=req.query.p;
+      console.log(data.length);
+
+      for(var n=0; n<data.length;n++){
+
+        console.log(qid);
+        console.log(data[n][0]);
+        console.log(data[n][1]);
+
+        connection.query('update question_history set correct="'+data[n][0]+'",total_student="'+data[n][1]+'" where q_id="'+qid+'"'),function(err,result,fields){
+          if(err) throw err;
+        };
       };
-    };
-  });
-  res.render('index',{});
+    })
+
+  form.parse(req);
+
+  res.redirect('/index');
 };
 
 
